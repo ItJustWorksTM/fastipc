fe488ac3dafc1ff1f7412562f06f6eed754bcc6d
AeroStun <24841307+AeroStun@users.noreply.github.com>
2025-04-20 01:48:25 +0200

    Add -Wpedantic

--- src/channel.hxx
+++ src/channel.hxx
@@ -13,7 +13,10 @@ struct ChannelSample final {
     std::size_t sequence_id{0U};
     std::size_t size{0U};
     std::chrono::system_clock::time_point timestamp{};
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
     std::byte payload[0];
+#pragma GCC diagnostic pop
 };
 
 struct ChannelPage final {
@@ -21,7 +24,10 @@ struct ChannelPage final {
     std::atomic_size_t next_seq_id{0U};
     std::atomic_uint64_t occupancy{0U};
     std::atomic_size_t latest_sample_index{0U};
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
     alignas(ChannelSample) std::byte samples_storage[0];
+#pragma GCC diagnostic pop
 
     std::size_t sample_size() const { return sizeof(ChannelSample) + max_payload_size; }
     std::size_t index_of(const ChannelSample& sample) const {
