aa65de07ae5cc662d7cb70a96ab327e1e86fd6d7
Ruthger Dijt <ruthgerd@dynexpr.net>
2026-03-10 22:36:00 +0100

    don't use move_only_function

--- src/co/scheduler.hxx
+++ src/co/scheduler.hxx
@@ -30,7 +30,7 @@ class Scheduler final {
   public:
     explicit Scheduler(io::Reactor* reactor = nullptr) : m_reactor(reactor) {}
 
-    void schedule(std::move_only_function<void()> fn) {
+    void schedule(std::function<void()> fn) {
         auto lock = std::scoped_lock{m_child_lock};
 
         m_queue.push(std::move(fn));
@@ -60,7 +60,7 @@ class Scheduler final {
 
   private:
     mutable std::recursive_mutex m_child_lock;
-    std::queue<std::move_only_function<void()>> m_queue;
+    std::queue<std::function<void()>> m_queue;
 
     io::Reactor* m_reactor;
 };
